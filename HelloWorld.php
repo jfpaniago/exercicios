7bd8ab37e67fc30af16b8361d559ec4e595e4e9f
jfpaniago <jf.paniago@hotmail.com>
2022-06-22 16:51:46 -0400

    a

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,4 +1,6 @@
 <?php
+//nesse Hello World eu fiz uma leitura dinamica das letras atraves de um laço de repetiçao
+//que ignora os caracteres '-'e printa apenas as letras
 $helloWorld = ['H','-','e','-','l','-','l','-','o','_','W','-','o','-','r','-','l','-','d','-','!'];
 $string = '';
 for ($i = 0; $i < count($helloWorld); $i++){
