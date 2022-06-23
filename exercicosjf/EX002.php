a258abb84ebf5398af8805f08562ba55eedf49eb
jfpaniago <jf.paniago@hotmail.com>
2022-06-23 13:49:38 -0400

    a

--- exercicosjf/EX002.php
+++ exercicosjf/EX002.php
@@ -1,6 +1,8 @@
+<?php
 //2 arrays e vamos verificar os numeros em comum.
 
 $array_0 - array (0,1,3,4,6,8);
 $array_1 = array (10,90,23,8,6);
 
 //Rodar dois loopings e verificar se existe em um e no outro.
+?>
